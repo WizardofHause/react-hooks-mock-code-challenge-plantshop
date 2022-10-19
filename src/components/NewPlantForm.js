c13f4559932db047ddac61552e50490f6c92921d
Jon Hause <jonrhause@gmail.com>
2022-10-19 14:12:13 -0600

    got the form working

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
-// import PlantCard from "./PlantCard";
+import React from "react";
 
-function NewPlantForm() {
-
-  const [plantData, setPlantData] = useState({
-    plantName: "",
-    plantImage: "",
-    plantPrice: ""
-  })
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("http://localhost:6001/plants", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(plantData)
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }
+function NewPlantForm({plantData, setPlantData, handleSubmit}) {
 
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form onSubmit={handleSubmit}>
-        <input id="plant-name" type="text" name="name" placeholder="Plant name" value={plantData.plantName} onChange={(e) => setPlantData({ ...plantData, plantName: e.target.value })} />
-        <input id="plant-image" type="text" name="image" placeholder="Image URL" value={plantData.plantImage} onChange={(e) => setPlantData({ ...plantData, plantImage: e.target.value })} />
-        <input id="plant-number" type="number" name="price" step="0.01" placeholder="Price" value={plantData.plantPrice} onChange={(e) => setPlantData({ ...plantData, plantPrice: e.target.value })} />
+        <input id="plant-name" type="text" name="name" placeholder="Plant name" value={plantData.name} onChange={(e) => setPlantData({ ...plantData, name: e.target.value })} />
+        <input id="plant-image" type="text" name="image" placeholder="Image URL" value={plantData.image} onChange={(e) => setPlantData({ ...plantData, image: e.target.value })} />
+        <input id="plant-number" type="number" name="price" step="0.01" placeholder="Price" value={plantData.price} onChange={(e) => setPlantData({ ...plantData, price: e.target.value })} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
@@ -36,24 +16,3 @@ function NewPlantForm() {
 }
 
 export default NewPlantForm;
-
-
-// function handleSubmit(e) {
-//   e.preventDefault();
-//   const newPlant = {
-//     plantName,
-//     plantImage,
-//     plantPrice,
-//   };
-//   console.log(newPlant)
-
-
-// fetch('http://localhost:6001/plants/', {
-//   method: "POST",
-//   headers: {
-//   "Content-Type": "application/json"
-// },
-//   body: JSON.stringify(newPlant)})
-//   .then(r => r.json())
-//   .then(newPlant => setPlants([...plants, newPlant]))
-//}
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -4,7 +4,26 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
   const [search, setSearch] = useState('')
+  const [plantData, setPlantData] = useState({
+    name: "",
+    image: "",
+    price: ""
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(plantData)
+    })
+      .then(response => response.json())
+      .then(plantData => setPlants([...plants, plantData]))
+  }
 
   const handleSearch = (e) => {
     setSearch(e.target.value)
@@ -12,9 +31,9 @@ function PlantPage() {
 
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm plantData={plantData} setPlantData={setPlantData} handleSubmit={handleSubmit}/>
       <Search search={search} handleSearch={handleSearch} />
-      <PlantList search={search} />
+      <PlantList search={search} plants={plants} setPlants={setPlants}/>
     </main>
   );
 }
